44e86a55df90a08532fac92ab28c2cf4680a7566
Omer Cohen <>
2021-06-29 14:32:04 +0300

    Fix

---
+++ testSDK/Pods/Target Support Files/Pods-testSDK-testSDKUITests/Pods-testSDK-testSDKUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testSDK_testSDKUITests : NSObject
+@end
+@implementation PodsDummy_Pods_testSDK_testSDKUITests
+@end
---
+++ testSDK/testSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testSDK
+//
+//  Created by Omer Cohen on 29/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
